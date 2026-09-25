60587d4b27cc7c26960337eb13c8ddc859b8ddc9
luongkhanhtuongthptbtdong-arch <luongkhanhtuong.thptbtdong@gmail.com>
2026-09-25 15:42:47 +0700

    config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,25 @@
-const firebaseConfig = {
+window.APP_CONFIG = {
+  mode: "firebase",
+  firebase: {
     apiKey: "AIzaSyCyvbYK5tjM2lHV-ccf_IR6PDj1Ch3IteU",
     authDomain: "qlytochuyenmonbtd.firebaseapp.com",
     projectId: "qlytochuyenmonbtd",
     storageBucket: "qlytochuyenmonbtd.firebasestorage.app",
     messagingSenderId: "1004814390389",
     appId: "1:1004814390389:web:edbd6d0b05d0aa921c143c"
+  },
+  prefix: "tcm_",
+  defaults: {
+    so: "SỞ GIÁO DỤC VÀ ĐÀO TẠO AN GIANG",
+    truong: "TRƯỜNG THPT BÌNH THẠNH ĐÔNG",
+    to: "TỔ TOÁN",
+    namHoc: "",
+    diaDanh: "An Giang",
+    hieuTruong: "",
+    toTruong: "",
+    thuKy: "",
+    nguongYeu: 5.0,
+    hanBaoCao: 25,
+    hanGiaoAn: 5
   }
+};
